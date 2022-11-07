e201f35a7c31eb791420b3cdaa9b3ef4c50b89a6
MaxAlm <max.almroth95@gmail.com>
2022-11-07 10:44:20 +0100

    Har gjort om API sidan från en quiz sida till en sida som hämtar bilder från NASAs API. Gjort mindre ändringar i BasicStyles css samt på HomePAge. På contact sidan så kan man enbart skicka iväg formuläret om alla rutor är ifyllda

--- umbracoprojekt/wwwroot/scripts/script.js
+++ umbracoprojekt/wwwroot/scripts/script.js
@@ -1,3 +1,15 @@
-﻿function goToTrivia() {
-    window.location.href = "/trivia-api";
+﻿function goToNasaPage() {
+    window.location.href = "/nasa-api";
+}
+
+function submitContactForm() {
+    var name = document.getElementById("name-input").value;
+    var mail = document.getElementById("mail-input").value;
+    var message = document.getElementById("message-input").value;
+
+    if (name != "" && mail != "" && message != "") {
+        alert("Ditt meddelande har skickats!");
+    } else {
+        return false;
+    }
 }
